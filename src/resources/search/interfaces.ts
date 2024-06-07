d3030e0dd2ef1639a6f0a6bee4c5df4f4ced7ff7
Genie <155519863+tl-genie@users.noreply.github.com>
2024-06-08 01:33:38 +0900

    feat: support image-to-video search api (#27)

--- src/resources/search/interfaces.ts
+++ src/resources/search/interfaces.ts
@@ -1,6 +1,10 @@
 export interface SearchOptions {
   indexId: string;
-  query: string | Record<string, any>;
+  query?: string | Record<string, any>;
+  queryText?: string;
+  queryMediaType?: 'image';
+  queryMediaFile?: Buffer | NodeJS.ReadableStream | string;
+  queryMediaUrl?: string;
   options?: ('visual' | 'conversation' | 'text_in_video' | 'logo')[];
   groupBy?: 'video' | 'clip';
   threshold?: 'high' | 'medium' | 'low';
