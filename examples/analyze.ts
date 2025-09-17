79eb57eb3614f9365f36e2760df23162bde7fa42
park-yeonhoo <park@twelvelabs.io>
2025-09-16 22:49:28 -0700

    format
    Signed-off-by: park-yeonhoo <park@twelvelabs.io>

--- examples/analyze.ts
+++ examples/analyze.ts
@@ -1,37 +1,37 @@
 import { TwelveLabs } from "twelvelabs-js";
 
 (async () => {
-    const client = new TwelveLabs({ apiKey: process.env.API_KEY });
+  const client = new TwelveLabs({ apiKey: process.env.TWELVE_LABS_API_KEY });
 
     const videoId = "<YOUR_VIDEO_ID>";
 
-    let summarizeResp = await client.summarize({
-        videoId,
-        type: "summary",
-    });
-    if (summarizeResp.summarizeType === "summary") {
-      console.log(`Summary: ${summarizeResp.summary}`);
+  let summarizeResp = await client.summarize({
+    videoId,
+    type: "summary",
+  });
+  if (summarizeResp.summarizeType === "summary") {
+    console.log(`Summary: ${summarizeResp.summary}`);
+  }
+
+  summarizeResp = await client.summarize({
+    videoId,
+    type: "chapter",
+  });
+  if (summarizeResp.summarizeType === "chapter") {
+    for (const chapter of summarizeResp.chapters!) {
+      console.log(
+        `Chapter: ${chapter.chapterNumber} ${chapter.chapterTitle} ${chapter.chapterSummary}`
+      );
     }
+  }
 
-    summarizeResp = await client.summarize({
-      videoId,
-      type: "chapter",
-    });
-    if (summarizeResp.summarizeType === "chapter") {
-      for (const chapter of summarizeResp.chapters!) {
-        console.log(
-          `Chapter: ${chapter.chapterNumber} ${chapter.chapterTitle} ${chapter.chapterSummary}`
-        );
-      }
-    }
+  const gist = await client.gist({
+    videoId,
+    types: ["title", "topic", "hashtag"],
+  });
+  console.log(`Gist: title=${gist.title} topics=${gist.topics} hashtags=${gist.hashtags}`);
 
-    const gist = await client.gist({
-        videoId,
-        types: ["title", "topic", "hashtag"],
-    });
-    console.log(`Gist: title=${gist.title} topics=${gist.topics} hashtags=${gist.hashtags}`);
-
-    // Basic streaming analyze example
+  // Basic streaming analyze example
   const textStream = await client.analyzeStream({
     videoId,
     prompt: "What happened?",
